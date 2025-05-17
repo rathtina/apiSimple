621226991435c068bf5b754c319f3a1f573adcc2
tinajr02 <141351707+tinajr02@users.noreply.github.com>
2025-05-18 02:45:43 +0700

    new

---
+++ src/main/java/org/springboot/authapi/Configuration/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package org.springboot.authapi.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Professional Spring Boot API")
+                        .version("1.0")
+                        .description("This is a professional-grade documentation for testing Spring Boot endpoints.")
+                        .contact(new Contact().name("API Support").email("support@example.com"))
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+}
