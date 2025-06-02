b1df318990f9e9793612672bdbfa3e9a6c20b273
rathtina <githubkhmer@gmail.com>
2025-06-02 09:13:31 +0700

    update table name

--- src/main/java/org/springboot/authapi/Enities/CartItem.java
+++ src/main/java/org/springboot/authapi/Enities/CartItem.java
@@ -5,7 +5,7 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name="cartItem")
+@Table(name="cart_items")
 public class CartItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/org/springboot/authapi/Enities/OrderItem.java
+++ src/main/java/org/springboot/authapi/Enities/OrderItem.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 
 @Data
 @Entity
-@Table(name = "orderItem")
+@Table(name = "order_item")
 public class OrderItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
