82e26c468f5babdde53e0d9dcabb6a69da39dbb6
rathtina <githubkhmer@gmail.com>
2025-06-03 17:24:33 +0700

    again

--- src/main/java/org/springboot/authapi/Enities/CartItem.java
+++ src/main/java/org/springboot/authapi/Enities/CartItem.java
@@ -12,11 +12,11 @@ public class CartItem {
     @Column(name = "Id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id")
     private Product product;
 
